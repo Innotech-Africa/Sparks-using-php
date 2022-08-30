bb9130b287f26ad054017d048d5b96d3e72ce33e
Char0738 <sharondlaminie07@gmail.com>
2022-08-30 09:39:47 +0200

    Create dashboard.php

---
+++ Sparks/dashboard.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+<div className='main'>
+        <div className="sidebar">
+           sidebar
+        </div>
+        <div className="container">
+         container
+
+        </div>
+    </div>
+</body>
+</html>
